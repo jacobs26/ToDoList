58ace7edbb80c3119e333d062d41673e99972c19
jacobs26 <jacobs26@illinois.edu>
2013-02-11 02:11:30 -0600

    add item added, added checkboxes for remove

--- my_script.js
+++ my_script.js
@@ -1,14 +1,25 @@
 $(document).ready(function(){
-	var rowCount=0;
-	if(rowCount===0)
-	{
-		$(".activelist").append(
-			"<tr class='rowa'> <td>Task 1</td> </tr>");
-			rowCount=1;
-	}
-	else{
-		$(".activelist").append(
-			"<tr class='rowb'> <td>Task 1</td> </tr>");
-		rowCount=0;
-	}
+	//$('h1').fadeOut();
+	var rowCount=1;
+	$('#enterButton').click(function(){
+			var taskEntry = $('input[name=listEntry]').val();
+
+			if(rowCount===0)
+			{
+				$(".activelist").append(
+				'<tr class="rowa"> <td>'+taskEntry+'<div class="uncheckbox"></div> </td> </tr>');
+				rowCount=1;
+			}
+			else{
+				$(".activelist").append(
+					'<tr class="rowb"> <td>'+taskEntry+' <div class="uncheckbox"></div> </td> </tr>');
+					rowCount=0;
+					}
+			$(".uncheckbox").on('click',function() {
+				console.log("clicked");				
+				$(this).toggleClass("checkedbox");
+			});
+		});
+	
 });
+
