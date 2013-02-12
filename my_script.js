88936e905046046823eb6292a33ee0fe9375f332
jacobs26 <jacobs26@illinois.edu>
2013-02-12 16:12:55 -0600

    reworking layout, dropping table menu concept

--- my_script.js
+++ my_script.js
@@ -1,37 +1,58 @@
 $(document).ready(function(){
-	$('#enterButton').click(function(){
-			var taskEntry = $('input[name=listEntry]').val();
-			if(taskEntry===""){
-				taskEntry="Blank Task";
-			}
-			$(".activearea").append(
-				'<tr class="rowa"> <td><div class="remove"></div>' +taskEntry+'<div class="uncheckbox"></div> </td> </tr>');
-	});
+	
 
-	$(document).on('click', '.uncheckbox',function() {
-		console.log("clicked");				
-		$(this).toggleClass("checkedbox");
-	});
-
-	$(document).on('click','.remove',function(){
-		$(this).parent().parent().remove();
-		});
-
+//Creates a new list and appeneds it to the active area
 $('#newListButton').click(function(){
+		$('#activearea table').hide();
 		var listName = $('input[name=listNameEntry]').val();
 			if(listName===""){
 				listName="List Name";
 			}
-		console.log('<div class=".listElement" id="'+listName+'>'+listName+'</div>');
-		$('#listPanel').append('<div class=".listElement" id="'+listName+'">'+listName+'</div>')
+		listNameId=listName.replace(/\s/g, "");
+		$('#activearea').append('<div id="'+listNameId+'"> </div>');
+		$('#'+listNameId).append('<h3>'+listName+'</h3>');
+		//$('#listPanel').append('<div class="listElement" id="'+listNameId+'">'+listName+'</div>');
+		//tableListId="table"+listNameId;
+		$('#'+listNameId).append('<table> </table>');
+		$('#'+listNameId).append('<div class="hideButton">'+listName+'</div>');
 	});
-	
-	
-	
 
+$('#enterButton').click(function(){
+			var taskEntry = $('input[name=listEntry]').val();
+			if(taskEntry===""){
+				taskEntry="Blank Task";
+			}
+			$("#"+tableListId).append(
+				'<tr><td class="rowa"><div class="remove"></div>'+taskEntry+'<div class="uncheckbox"></div> </td> </tr>');
+		});
+
+		
+$(document).on('click','.listElement',function(){
+	var currentID=$(this).attr('id');
+	$('#activearea').children().hide();
+	$('#'+currentID).show();
+});
+
+
+
+
+
+//This toggles the check boxes between checked and unchecked
+	$(document).on('click', '.uncheckbox',function() {			
+		$(this).toggleClass("checkedbox");
+	});
+//Removes  the checked boxes of each item in class .checkedbox
 $('#doneText').click(function(){
 		$('.checkedbox').parent().parent().remove();
 	});
+//This removes the table row of each clicked remove button
+	$(document).on('click','.remove',function(){
+		$(this).parent().parent().remove();
+		});
+
+
+
+
 
 });
 
