b8879e12095187986bea357ebbafe2b6ebb28bb1
jacobs26 <jacobs26@illinois.edu>
2013-02-11 23:16:12 -0600

    changed table structure in html, added list box

--- my_script.js
+++ my_script.js
@@ -1,28 +1,33 @@
 $(document).ready(function(){
-	//$('h1').fadeOut();
 	$('#enterButton').click(function(){
 			var taskEntry = $('input[name=listEntry]').val();
 			if(taskEntry===""){
 				taskEntry="Blank Task";
 			}
-			$(".activelist").append(
+			$(".activearea").append(
 				'<tr class="rowa"> <td><div class="remove"></div>' +taskEntry+'<div class="uncheckbox"></div> </td> </tr>');
 	});
 
-	
 	$(document).on('click', '.uncheckbox',function() {
 		console.log("clicked");				
 		$(this).toggleClass("checkedbox");
 	});
+
 	$(document).on('click','.remove',function(){
 		$(this).parent().parent().remove();
 		});
-	$('#nameListButton').click(function(){
-			var listName = $('input[name=listNameEntry]').val();
+
+$('#newListButton').click(function(){
+		var listName = $('input[name=listNameEntry]').val();
 			if(listName===""){
 				listName="List Name";
 			}
+		console.log('<div class=".listElement" id="'+listName+'>'+listName+'</div>');
+		$('#listPanel').append('<div class=".listElement" id="'+listName+'">'+listName+'</div>')
 	});
+	
+	
+	
 
 $('#doneText').click(function(){
 		$('.checkedbox').parent().parent().remove();
